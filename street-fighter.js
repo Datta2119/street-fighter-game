4a0e86c9ab5d6bc54d134c5f095fbb6f11bc3db1
Datta2119 <asbishway2119@gmail.com>
2022-12-14 15:02:30 +0600

    update: after match finish attack & heal issue fixed

--- street-fighter.js
+++ street-fighter.js
@@ -7,7 +7,7 @@ let p2HealthDiv = document.getElementById("p2Health");
 let resultDiv = document.getElementById("result");
 
 let resetButton = document.getElementById("reset");
-let playButton = document.getElementById("play");
+let simulateButton = document.getElementById("play");
 
 // ** Check if either players health is  0 and if it is, then update isOver to true **
 const updateGame = (p1, p2, gameState) => {
@@ -36,40 +36,46 @@ class Player {
 
     // ** Attack an enemy with a random number from 0 to YOUR attackDmg bonus **
     strike(player, enemy, attackDamage) {
-        // Get random number between 1 - 10 and that is damageAmount
-        let damageAmount = Math.ceil(Math.random() * attackDamage)
 
-        let afterDamageHealth = enemy.health - damageAmount
+        if (game.isOver == false) {
+            // Get random number between 1 - 10 and that is damageAmount
+            let damageAmount = Math.ceil(Math.random() * attackDamage)
 
-        if (afterDamageHealth >= 0) {
-            // Subtract the enemy health with the damageAmount
-            enemy.health -= damageAmount
+            let afterDamageHealth = enemy.health - damageAmount
 
-            //  Update the game and DOM with updateGame
-            updateGame(p1, p2, gameState)
+            if (afterDamageHealth >= 0) {
+                // Subtract the enemy health with the damageAmount
+                enemy.health -= damageAmount
+
+                //  Update the game and DOM with updateGame
+                updateGame(p1, p2, gameState)
+            }
         }
+        
 
         //  Return a message of 'player name attacks enemy name for damageAmount'
-        return `${player.name} attacks ${enemy.name} for ${damageAmount} HP!`
+        // return `${player.name} attacks ${enemy.name} for ${damageAmount} HP!`
     }
 
     // ** Heal the player for random number from  1 to 5 **
     heal(player) {
         // Get random number between 1 - 5 and store that in healAmount
         // debugger
-        let healAmount = Math.ceil(Math.random() * 5)
-        // Add healAmount to players health
-        let afterHealAmount = player.health + healAmount
 
-        if (afterHealAmount <= 100) {
-
+        if (game.isOver == false) {
+            let healAmount = Math.ceil(Math.random() * 5)
             // Add healAmount to players health
-            player.health += healAmount
+            let afterHealAmount = player.health + healAmount
 
-            //  Update the game and DOM with updateGame
-            updateGame(p1, p2, gameState)
+            if (afterHealAmount <= 100) {
+
+                // Add healAmount to players health
+                player.health += healAmount
+
+                //  Update the game and DOM with updateGame
+                updateGame(p1, p2, gameState)
+            }
         }
-        return `${player} heals for ${healAmount} HP!`
     }
 }
 
@@ -80,29 +86,36 @@ class Game {
 
     // ** If the game is over and a player has 0 health declare the winner! **
     declareWinner(isOver, p1, p2) {
-        let message
+        let message;
 
-        // If isOver is true AND p1 health is <= 0 then update message variable  to 'p1 WINS!'
-        if (isOver == true && p1.health <= 0) {
-            message = `${p2.name} Wins!`
+        try {
+    
+            // If isOver is true AND p1 health is <= 0 then update message variable  to 'p1 WINS!'
+            if (isOver == true && p1.health <= 0) {
+                message = `${p2.name} Wins!`;
+            }
+
+            // Else if isOver is true AND p2 health is <= 0 then update message variable  to 'p2 WINS!'
+            else if (isOver == true && p2.health <= 0) {
+                message = `${p1.name} Wins!`
+            }
+
+            else if (isOver == true && p1.health < p2.health) {
+                message = `${p2.name} Wins!`
+            }
+
+            else if (isOver == true && p2.health < p1.health) {
+                message = `${p1.name} Wins!`
+            }
+
+            else if (isOver == true && p1.health == p2.health) {
+                message = "Draw!";
+            }
+        } catch (error) {
+            throw new Error(error);
         }
 
-        // Else if isOver is true AND p2 health is <= 0 then update message variable  to 'p2 WINS!'
-        else if (isOver == true && p2.health <= 0) {
-            message = `${p1.name} Wins!`
-        }
-
-        else if (isOver == true && p1.health < p2.health) {
-            message = `${p2.name} Wins!`
-        }
-
-        else if (isOver == true && p2.health < p1.health) {
-            message = `${p1.name} Wins!`
-        }
-
-        else if (isOver == true && p1.health == p2.health) {
-            message = "Draw!"
-        }
+        
 
         console.log(isOver, p1.health, p2.health)
 
@@ -139,23 +152,22 @@ class Game {
     }
 }
 
-const newPlayer1 = new Player("Bishway", 100, 10)
-const newPlayer2 = new Player("Sumon", 100, 10)
+const player1 = new Player("Bishway", 100, 10)
+const player2 = new Player("Sumon", 100, 10)
 
-const p1 = newPlayer1
-const p2 = newPlayer2
+const p1 = player1
+const p2 = player2
 
 const game = new Game()
 updateGame(p1, p2)
 const gameState = game.isOver
 
 // ** Add a click listener to the simulate button that runs the play() method on click and pass in the players **
-playButton.onclick = () => {
+simulateButton.onclick = () => {
     resultDiv.innerText = game.play(p1, p2)
 }
 
-
-// Player 1 game control 👉 attack
+// *** Adding Event Listener in Attack Button for player1 ***
 document.addEventListener("keydown", (event) => {
     // if you press Q AND the enemy health is greater than 0 AND isOver is still false then strike()
     if (event.key === "q" && p2.health > 0 && game.isOver == false) {
@@ -164,17 +176,16 @@ document.addEventListener("keydown", (event) => {
     }
 })
 
-// Player 1 game control 👉 heal
+// *** Adding Event Listener in Heal Button for player1 ***
 document.addEventListener("keydown", (event) => {
     // if you press Q AND the enemy health is greater than 0 AND isOver is still false then strike()
-    if (event.key === "a" && 0 <= p1.health <= 100) {
+    if (event.key === "a" && 0 <= p1.health <= 100 && game.isOver == false) {
         p1.heal(p1)
         document.getElementById("p1Heal").play()
     }
 })
 
-
-// Player 2 game control 👉 attack
+// *** Adding Event Listener in Attack Button for player2 ***
 document.addEventListener("keydown", (event) => {
     if (event.key === "p" && p1.health > 0 && game.isOver == false) {
         p2.strike(p2, p1, p2.attackDamage)
@@ -182,7 +193,7 @@ document.addEventListener("keydown", (event) => {
     }
 })
 
-// Player 2 game control 👉 heal
+// *** Adding Event Listener in Attack Button for player1 ***
 document.addEventListener("keydown", (event) => {
     // if you press Q AND the enemy health is greater than 0 AND isOver is still false then strike()
     if (event.key === "l" && 0 <= p2.health <= 100) {
